cb7f573a81a5d634cf134f0a899727f8a18ccd40
Agchai52 <shuangzhang0809@gmail.com>
2021-03-09 16:24:13 -0500

    Refactor: compute metric GOPRO

--- compute_Kohler.py
+++ compute_Kohler.py
@@ -10,20 +10,8 @@ from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
 
-
-# def psnr(img1, img2):
-#     mse = np.mean((img1 - img2) ** 2)
-#     if mse == 0:
-#         return 100
-#     PIXEL_MAX = 255.0
-#     return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
-
-
-deblu_root = './res_kohler'  # _all_deblurred'
-sharp_root = './datasets/test_kohler'  # _all'
-
-# deblu_root = './res_GOPRO_NB'
-# sharp_root = './datasets/test_sharpm'
+deblu_root = './res_kohler'
+sharp_root = './datasets/Kohler_multi1'
 
 deblu_list = os.listdir(deblu_root)
 sharp_list = os.listdir(sharp_root)
@@ -36,8 +24,14 @@ SSIM_all = []
 count_k = 1
 psnr_k = []
 ssim_k = []
+vif_k = []
+vsi_k = []
+haar_k = []
 kernel_p = []
 kernel_s = []
+kernel_f = []
+kernel_v = []
+kernel_h = []
 VIF_all = []
 VSI_all = []
 Haar_all = []
@@ -54,7 +48,7 @@ for n, item in enumerate(sharp_list):
         img_deblu = cv2.imread(path_deblu, cv2.IMREAD_COLOR).astype(np.float)
         img_sharp = cv2.imread(path_sharp, cv2.IMREAD_COLOR).astype(np.float)
 
-        _, _, img_sharp = np.split(img_sharp, 3, axis=1)
+        # img_blur, img_sharp = np.split(img_sharp, 2, axis=1)
 
         # cv2.imwrite('img_blur.png', img_blur)
         #cv2.imwrite('img_deblu.png', img_deblu)
@@ -89,41 +83,60 @@ for n, item in enumerate(sharp_list):
         vsi_n = piq.vsi(deblu_ts, sharp_ts)
         haar_n = piq.haarpsi(deblu_ts, sharp_ts)
 
-        #
-        # if count_k < 198:
-        #     psnr_k.append(psnr_n)
-        #     ssim_k.append(ssim_n)
-        #     count_k += 1
-        # elif count_k == 198:
-        #     psnr_k.append(psnr_n)
-        #     ssim_k.append(ssim_n)
-        #     kernel_p.append(max(psnr_k))
-        #     kernel_s.append(max(ssim_k))
-        #     psnr_k = []
-        #     ssim_k = []
-        #     count_k = 1
+        # For Kohler dataset
+        if count_k < 198:
+            psnr_k.append(psnr_n)
+            ssim_k.append(ssim_n)
+            vif_k.append(vif_n)
+            vsi_k.append(vsi_n)
+            haar_k.append(haar_n)
+            count_k += 1
+        elif count_k == 198:
+            psnr_k.append(psnr_n)
+            ssim_k.append(ssim_n)
+            vif_k.append(vif_n)
+            vsi_k.append(vsi_n)
+            haar_k.append(haar_n)
+            kernel_p.append(max(psnr_k))
+            kernel_s.append(max(ssim_k))
+            kernel_f.append(max(vif_k))
+            kernel_v.append(max(vsi_k))
+            kernel_h.append(max(haar_k))
 
-        PSNR_all.append(psnr_n)
-        SSIM_all.append(ssim_n)
-        VIF_all.append(vif_n)
-        VSI_all.append(vsi_n)
-        Haar_all.append(haar_n)
+            psnr_k = []
+            ssim_k = []
+            vif_k = []
+            vsi_k = []
+            haar_k = []
+
+            count_k = 1
+
+        # For other datasets
+        # PSNR_all.append(psnr_n)
+        # SSIM_all.append(ssim_n)
+        # VIF_all.append(vif_n)
+        # VSI_all.append(vsi_n)
+        # Haar_all.append(haar_n)
     else:
         continue
 
-PSNR = np.mean(PSNR_all)
-SSIM = np.mean(SSIM_all)
-VIF = np.mean(VIF_all)
-VSI = np.mean(VSI_all)
-Haar = np.mean(Haar_all)
-# print(PSNR_all)
-# print(SSIM_all)
-print("ave PSNR = ", PSNR)
-print("ave SSIM = ", SSIM)
-print("ave VIF = ", VIF)
-print("ave VSI = ", VSI)
-print("ave Haar = ", Haar)
-# print('For Kohler dataset')
+# For other dataset
+# PSNR = np.mean(PSNR_all)
+# SSIM = np.mean(SSIM_all)
+# VIF = np.mean(VIF_all)
+# VSI = np.mean(VSI_all)
+# Haar = np.mean(Haar_all)
 #
-# print(np.mean(kernel_p))
-# print(np.mean(kernel_s))
+# print("ave PSNR = ", PSNR)
+# print("ave SSIM = ", SSIM)
+# print("ave VIF = ", VIF)
+# print("ave VSI = ", VSI)
+# print("ave Haar = ", Haar)
+
+# For Kohler dataset
+#
+print("ave PSNR = ", np.mean(kernel_p))
+print("ave SSIM = ", np.mean(kernel_s))
+print("ave VIF = ", np.mean(kernel_f))
+print("ave VSI = ", np.mean(kernel_v))
+print("ave Haar = ", np.mean(kernel_h))
