b4f9beed904e918e959967d1abfd11caf4c8bb65
Agchai52 <shuangzhang0809@gmail.com>
2021-04-04 13:26:48 -0400

    Refactor: test real MPR

--- test_aided.py
+++ test_aided.py
@@ -50,7 +50,7 @@ os.makedirs(out_dir, exist_ok=True)
 # For nonBlind only
 # f_test = open("./dataset/AidedDeblur/test_instance_names.txt", "r")
 # For nonBlind real images only
-f_test = open("./dataset/real_images/real_images_names.txt", "r")
+f_test = open("./dataset/real_images/real_image_names.txt", "r")
 imgsName = f_test.readlines()
 imgsName = [line.rstrip() for line in imgsName]
 f_test.close()
